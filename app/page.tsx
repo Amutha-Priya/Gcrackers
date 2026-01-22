2eac9f27ccedd9361efd2919dc10f3a4ab1ee12c
Amuthapriya <amuthacse29@gmail.com>
2026-01-22 17:44:14 +0530

    220126done

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
-import HeaderBanner from "./components/TitleBanner/Header";
+// import HeaderBanner from "./components/TitleBanner/Header";
 import Navbarcomponents from "./components/Navbar/Navbar";
 import Banner from "./components/Banner/banner"
 import Ordersummary from "./components/Ordersummary/Ordersummary"
-import Footer from "./components/footer/footer"
+// import Footer from "./components/footer/footer"
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-zinc-50 font-sans">
-      <HeaderBanner />
+      {/* <HeaderBanner /> */}
       <Navbarcomponents />
       <Banner />
       <Ordersummary />
-      <Footer />
+      {/* <Footer /> */}
     
     </div>
   );
